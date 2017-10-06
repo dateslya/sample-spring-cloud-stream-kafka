48137941f91599aa3f4140e893b127264edab6eb
Dmitry_Teslya <d.a.teslya@gmail.com>
2017-10-06 22:35:10 +0400

    Init project

---
+++ src/main/java/ru/sample/spring/cloud/stream/kafka/SampleSpringCloudStreamKafkaApplication.java
@@ -0,0 +1,12 @@
+package ru.sample.spring.cloud.stream.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleSpringCloudStreamKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleSpringCloudStreamKafkaApplication.class, args);
+	}
+}
---
+++ src/test/java/ru/sample/spring/cloud/stream/kafka/SampleSpringCloudStreamKafkaApplicationTests.java
@@ -0,0 +1,16 @@
+package ru.sample.spring.cloud.stream.kafka;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SampleSpringCloudStreamKafkaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
