8f22c9d7d099a4428939d1084c16909bd7ff7867
Dmitry_Teslya <d.a.teslya@gmail.com>
2017-10-07 03:34:46 +0400

    First sample

--- src/main/java/ru/sample/spring/cloud/stream/kafka/SampleSpringCloudStreamKafkaApplication.java
+++ src/main/java/ru/sample/spring/cloud/stream/kafka/SampleSpringCloudStreamKafkaApplication.java
@@ -1,12 +1,43 @@
 package ru.sample.spring.cloud.stream.kafka;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.cloud.stream.messaging.Sink;
+import org.springframework.cloud.stream.messaging.Source;
+import org.springframework.integration.support.MessageBuilder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.io.UnsupportedEncodingException;
+
+@RestController
+@EnableBinding({Sink.class, Source.class})
 @SpringBootApplication
 public class SampleSpringCloudStreamKafkaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SampleSpringCloudStreamKafkaApplication.class, args);
-	}
+    private static final Logger LOG = LoggerFactory.getLogger(SampleSpringCloudStreamKafkaApplication.class);
+
+    @Autowired
+    private Source forward;
+
+    public static void main(String[] args) {
+        SpringApplication.run(SampleSpringCloudStreamKafkaApplication.class, args);
+    }
+
+    @GetMapping("/send")
+    public void send(@RequestParam String message) throws UnsupportedEncodingException {
+        LOG.info("Forward message: {}", message);
+        forward.output().send(MessageBuilder.withPayload(message).build());
+    }
+
+    @StreamListener(Sink.INPUT)
+    public void handle(String message) {
+        LOG.info("Handle message: {}", message);
+    }
 }
--- src/test/java/ru/sample/spring/cloud/stream/kafka/SampleSpringCloudStreamKafkaApplicationTests.java
+++ src/test/java/ru/sample/spring/cloud/stream/kafka/SampleSpringCloudStreamKafkaApplicationTests.java
@@ -1,16 +1,39 @@
 package ru.sample.spring.cloud.stream.kafka;
 
+import org.junit.BeforeClass;
+import org.junit.ClassRule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.kafka.test.rule.KafkaEmbedded;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
+@ActiveProfiles("embedded-kafka")
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 public class SampleSpringCloudStreamKafkaApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @ClassRule
+    public static KafkaEmbedded embeddedKafka = new KafkaEmbedded(1);
 
+    private static TestRestTemplate restTemplate = new TestRestTemplate();
+
+    @BeforeClass
+    public static void setUp() {
+        System.setProperty("spring.embedded.kafka.zkNodes", embeddedKafka.getZookeeperConnectionString());
+    }
+
+    @Test
+    public void contextLoads() throws InterruptedException {
+        for (int i = 0; i < 100; i++) {
+            try {
+                Thread.sleep(5 * 1000);
+                restTemplate.getForObject("http://localhost:8080/send?message=Hello-" + i, String.class);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
